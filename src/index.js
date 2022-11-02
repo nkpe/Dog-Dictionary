02968aea869355aba6400f2e19aef85142b69acc
nkpe <naomi.pearse@gmail.com>
2022-11-02 11:26:04 +0000

    dog images returned

--- src/index.js
+++ src/index.js
@@ -6,14 +6,24 @@ const main = document.getElementById('main');
 
 let getDogBreeds = () => {
     fetch("https://dog.ceo/api/breeds/list/all")
-    .then(res => {
-        const dogBreedsData = res.json();
-        return dogBreedsData
-    })
-    .then(dogBreedsData => {
-        const dogBreeds = Object.getOwnPropertyNames(dogBreedsData.message);
-        console.log(dogBreeds);
-    })
+        .then(response => {
+            const dogBreedsData = response.json();
+            return dogBreedsData
+        })
+        .then(dogBreedsData => {
+            const dogBreeds = Object.getOwnPropertyNames(dogBreedsData.message);
+            for (let breed of dogBreeds) {
+                fetch(`https://dog.ceo/api/breed/${breed}/images`)
+                    .then(res => {
+                        let breedPictures = res.json();
+                        return breedPictures
+                    }).then(breedPictures => {
+                        let breedPicture = breedPictures.message[0];
+                        return breedPicture
+                    });
+                    //assign breed key to breedPicture
+            }
+        })
 }
 
 getDogBreeds();
