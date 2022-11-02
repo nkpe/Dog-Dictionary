9ccc67a742baccb8e219983997f9aff4515734ad
nkpe <naomi.pearse@gmail.com>
2022-11-02 10:43:00 +0000

    dog breeds returned from API in array

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Dog-Dictionary
 Dictionary of Dog breeds (data source : https://dog.ceo/dog-api/)
+
+## How to run and view it
--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 'use strict';
 
-console.log("index.js working")
+console.log("index.js working");
+
+const main = document.getElementById('main');
+
+let getDogBreeds = () => {
+    fetch("https://dog.ceo/api/breeds/list/all")
+    .then(res => {
+        const dogBreedsData = res.json();
+        return dogBreedsData
+    })
+    .then(dogBreedsData => {
+        const dogBreeds = Object.getOwnPropertyNames(dogBreedsData.message);
+        console.log(dogBreeds);
+    })
+}
+
+getDogBreeds();
